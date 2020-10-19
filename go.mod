6000a57f4b09788f0c4c0ecb18d1d1ba3ec5b2e1
erez weiss <erezw@gett.com>
2020-10-19 13:13:10 +0300

    delete shares with a provided email address

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/ruggertech/unshare-google-drive
+
+go 1.13
+
+require (
+	golang.org/x/net v0.0.0-20201002202402-0a1ea396d57c
+	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43
+	google.golang.org/api v0.32.0
+)
--- main.go
+++ main.go
@@ -1,73 +1,16 @@
 package main
 
 import (
-	"encoding/json"
+	"context"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"os"
-
-	"golang.org/x/net/context"
-	"golang.org/x/oauth2"
+	"github.com/ruggertech/unshare-google-drive/pkg/auth"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/drive/v3"
+	"google.golang.org/api/option"
+	"io/ioutil"
+	"log"
+	"strings"
 )
-
-// Retrieve a token, saves the token, then returns the generated client.
-func getClient(config *oauth2.Config) *http.Client {
-	// The file token.json stores the user's access and refresh tokens, and is
-	// created automatically when the authorization flow completes for the first
-	// time.
-	tokFile := "token.json"
-	tok, err := tokenFromFile(tokFile)
-	if err != nil {
-		tok = getTokenFromWeb(config)
-		saveToken(tokFile, tok)
-	}
-	return config.Client(context.Background(), tok)
-}
-
-// Request a token from the web, then returns the retrieved token.
-func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
-	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-	fmt.Printf("Go to the following link in your browser then type the "+
-		"authorization code: \n%v\n", authURL)
-
-	var authCode string
-	if _, err := fmt.Scan(&authCode); err != nil {
-		log.Fatalf("Unable to read authorization code %v", err)
-	}
-
-	tok, err := config.Exchange(context.TODO(), authCode)
-	if err != nil {
-		log.Fatalf("Unable to retrieve token from web %v", err)
-	}
-	return tok
-}
-
-// Retrieves a token from a local file.
-func tokenFromFile(file string) (*oauth2.Token, error) {
-	f, err := os.Open(file)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	tok := &oauth2.Token{}
-	err = json.NewDecoder(f).Decode(tok)
-	return tok, err
-}
-
-// Saves a token to a file path.
-func saveToken(path string, token *oauth2.Token) {
-	fmt.Printf("Saving credential file to: %s\n", path)
-	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		log.Fatalf("Unable to cache oauth token: %v", err)
-	}
-	defer f.Close()
-	json.NewEncoder(f).Encode(token)
-}
 
 func main() {
 	b, err := ioutil.ReadFile("credentials.json")
@@ -76,28 +19,77 @@ func main() {
 	}
 
 	// If modifying these scopes, delete your previously saved token.json.
-	config, err := google.ConfigFromJSON(b, drive.DriveMetadataReadonlyScope)
+	config, err := google.ConfigFromJSON(b, drive.DriveScope, drive.DriveFileScope)
 	if err != nil {
 		log.Fatalf("Unable to parse client secret file to config: %v", err)
 	}
-	client := getClient(config)
+	client := auth.GetClient(config)
 
-	srv, err := drive.New(client)
+	ctx := context.Background()
+	srv, err := drive.NewService(ctx, option.WithHTTPClient(client))
 	if err != nil {
 		log.Fatalf("Unable to retrieve Drive client: %v", err)
 	}
 
-	r, err := srv.Files.List().PageSize(10).
-		Fields("nextPageToken, files(id, name)").Do()
-	if err != nil {
-		log.Fatalf("Unable to retrieve files: %v", err)
-	}
-	fmt.Println("Files:")
-	if len(r.Files) == 0 {
-		fmt.Println("No files found.")
-	} else {
-		for _, i := range r.Files {
-			fmt.Printf("%s (%s)\n", i.Name, i.Id)
+	firstCall := true
+	nextPageToken := ""
+	filesGlobalCounter := 0
+	pageGlobalCounter := 0
+	skipGapForPrintPurposes := 400
+	pageSize := int64(200)
+	// TODO: Change the following email to unshare everything in google drive with the person
+	emailToUnshare := "someonesEmail@gmail.com"
+
+	for nextPageToken != "" || firstCall {
+		firstCall = false
+		r, err := srv.Files.List().PageSize(pageSize).PageToken(nextPageToken).
+			//Fields("*").Do()
+			Fields("nextPageToken, files(id, name, permissions)").Do()
+		if err != nil {
+			log.Fatalf("Unable to retrieve files: %v", err)
+		}
+		nextPageToken = r.NextPageToken
+		pageGlobalCounter += 1
+		fmt.Printf("Page %d:\n", pageGlobalCounter)
+		if len(r.Files) == 0 {
+			fmt.Println("No files found.")
+		} else {
+			for _, i := range r.Files {
+				filesGlobalCounter += 1
+				if filesGlobalCounter%skipGapForPrintPurposes == 0 {
+					fmt.Printf("%s (%s) \n", i.Name, i.Id)
+				}
+				for _, per := range i.Permissions {
+					if strings.Contains(per.EmailAddress, emailToUnshare) {
+						fmt.Printf("ALERT - fileName: \"%s\", fileId: \"%s\", permissionId: \"%s\" , role: %s, email: %s (%s)\n", i.Name, i.Id, per.Id, per.Role, per.EmailAddress, per.DisplayName)
+						// before deleting permission, copy un-owned items
+						if per.Role == "owner" {
+							copiedFile := &drive.File{
+								Name: i.Name,
+							}
+
+							res, err := srv.Files.Copy(i.Id, copiedFile).Do()
+							if err != nil {
+								fmt.Printf("An error occurred when copying: %v+\n", err)
+							} else {
+								fmt.Printf("copied file: %v+\n", res)
+							}
+						}
+
+						fmt.Printf("Deleting the permission\n")
+						delErr := srv.Permissions.Delete(i.Id, per.Id).Do()
+						if delErr != nil {
+							fmt.Printf("An error occurred: %v+\n", delErr)
+						}
+					}
+
+					// every 20 files print to show progress
+					if filesGlobalCounter%skipGapForPrintPurposes == 0 {
+						fmt.Printf("Normal - id: %s, role: %s, email: %s (%s)\n", per.Id, per.Role, per.EmailAddress, per.DisplayName)
+					}
+				}
+			}
 		}
 	}
+
 }
